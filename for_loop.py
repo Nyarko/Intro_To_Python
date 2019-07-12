80e71c9cfe2b8374d046e82b711670e1309e0285
Nyarko <nyarko.agyekum@yahoo.com>
2019-07-12 14:11:53 +0000

    Uploading changes

---
+++ for_loop.py
@@ -0,0 +1,17 @@
+#code 1
+for i in range(0,5):
+	print (i)
+
+print ()
+
+#code 2
+for j in [1, 2, 3]:
+	print (j)
+
+print()
+
+#code 3: using range to do the same as in 2
+for j in range(1,4):
+	print (j)
+	n = type(range)
+	print (n)
--- while_loop_2.py
+++ while_loop_2.py
@@ -6,8 +6,22 @@ def evens(n1,n2):
 	while (n1 < (n2 - 1)):
 		n1 = n1 + 1
 		if (n1 % 2 == 0):
-			return n1
-	return 0
+			print (n1)
+	return 'Done'
 
-
+print('The even numbers between', n1, 'and ', n2, 'are:')
 print (evens(n1,n2))
+
+print()
+
+#prints the reverse
+def reverse_evens(n1,n2):
+
+	while (n2 > n1):
+		n2 = n2 - 1
+		if (n2 % 2 == 0):
+			print (n2)
+	return 'Done'
+
+print('The even numbers between', n1, 'and ', n2, 'in reverse are:')
+print (reverse_evens(n1,n2))
